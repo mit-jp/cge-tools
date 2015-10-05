7bdac9cb6bbbcc2ccd1537a7aa370a775a8e94b9
Sarah Bird <sarah@bonvaya.com>
2015-10-05 13:51:23 -0400

    Get plots rendering responsively

--- crem_presentation/site/content/viz/plot_1.py
+++ crem_presentation/site/content/viz/plot_1.py
@@ -1,10 +1,11 @@
 from collections import OrderedDict
 from bokeh._legacy_charts import Area
 from bokeh.embed import components
+from bokeh.palettes import Spectral3
 from jinja2 import Template
 
 
-def _get():
+def _get(width=600):
     xyvalues = OrderedDict(
         python=[2, 3, 7, 5, 26, 221, 44, 233, 254, 265, 266, 267, 120, 111],
         pypy=[12, 33, 47, 15, 126, 121, 144, 233, 254, 225, 226, 267, 110, 130],
@@ -15,6 +16,7 @@ def _get():
         xyvalues, title="Responsive Area Chart",
         xlabel='time', ylabel='memory',
         stacked=True, legend="top_left",
+        width=width, palette=Spectral3,
         responsive=True
     )
 
@@ -22,17 +24,21 @@ def _get():
 
 
 def render():
-    plot = _get()
+    a1 = _get(900)
+    a2 = _get(300)
 
     # Define our html template for out plots
     template = Template('''<!DOCTYPE html>
-        <h5>Resize the window to see some plots resizing</h5>
-        <p>Red - pan with responsive</p>
-        {{ plot_div }}
+        <div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--6-col mdl-grid">
+        {{ plot_div.a1 }}
+        </div>
+        <div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--6-col mdl-grid">
+        {{ plot_div.a2 }}
+        </div>
         {{ plot_script }}
     ''')
 
-    script, div = components(plot)
+    script, div = components(dict(a1=a1, a2=a2))
 
     html = template.render(plot_script=script, plot_div=div)
     return html

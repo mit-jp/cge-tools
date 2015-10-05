b942590524ed15044613112882fb113daa106400
Sarah Bird <sarah@bonvaya.com>
2015-10-05 04:20:48 -0400

    Add pelican static site generator

---
+++ crem_presentation/pelican/pelicanconf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'CECP'
+SITENAME = 'C-REM'
+SITEURL = ''
+SITESUBTITLE = 'Bacon ipsum dolor amet jerky porchetta hamburger flank pastrami salami chicken biltong picanha frankfurter ribeye.'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'mdl'
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['mdl_renderer',]

f360153b426d6f8ecf8e44798065d0f7d1595d3d
Sarah Bird <sarah@bonvaya.com>
2015-10-08 12:32:52 -0400

    Add health_impacts map

---
+++ crem_presentation/site/content/viz/health_impacts_by_province.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*- #
+from bokeh.embed import components
+
+from .charts import get_provincial_scenario_line_plot
+from .maps import get_province_maps_by_parameter, _add_province_callback, _add_region_callback
+from .constants import provinces
+from .utils import env
+
+
+def render_he_maps():
+    return 'hello'
+
+
+def render_col_map():
+    plot, col_province_map, province_map, region_map = _get()
+    template = env.get_template('viz/health_impacts_by_province.html')
+    script, div = components(
+        dict(plot=plot, col_province_map=col_province_map, province_map=province_map, region_map=region_map),
+        wrap_plot_info=False
+    )
+    return template.render(plot_script=script, plot_div=div)
+
+
+def _get():
+    parameter = 'PM25_exposure'
+    plot, line_renderers, text_renderers = get_provincial_scenario_line_plot(
+        parameter=parameter,
+        y_ticks=[0, 450, 900],
+        plot_width=600,
+    )
+    region_map, province_map, col_province_map, source = get_province_maps_by_parameter(
+        parameter=parameter, parameter_name='PM2.5 exposure'
+    )
+
+    prefixed_renderers = {}
+    for province in provinces.keys():
+        prefixed_renderers['line_%s' % province] = line_renderers[province]
+        prefixed_renderers['text_%s' % province] = text_renderers[province]
+
+    province_map = _add_province_callback(province_map, prefixed_renderers, source)
+    col_province_map = _add_province_callback(col_province_map, prefixed_renderers, source)
+    region_map = _add_region_callback(region_map, prefixed_renderers, source)
+    return (plot, col_province_map, province_map, region_map)

e18a10d5884f1b8ef2e2717d28d82070077a6a5a
Sarah Bird <sarah@bonvaya.com>
2015-11-26 09:44:05 -0800

    Add homepage menu items

--- crem_presentation/site/content/viz/home.py
+++ crem_presentation/site/content/viz/home.py
@@ -1,10 +1,44 @@
 # -*- coding: utf-8 -*- #
-from bokeh.embed import components
-
-from ._maps import get_co2_2030_4_vs_bau_change_map, get_col_2010_map
 from .__utils import env
 
 
 def render():
     template = env.get_template('viz/home_page.html')
-    return template.render()
+    pages = [
+        {
+            "title": "Air pollution",
+            "link": "air-pollution",
+            "text": "China has a unique opportunity to clean up CO₂ and local air pollution at the same time."
+        },
+        {
+            "title": "Energy mix",
+            "link": "energy-mix",
+            "text": "China's target of 20% of energy from non-fossil sources (wind, solar, hydro, and nuclear power) is met in 2030 in the 4% scenario."
+        },
+        {
+            "title": "National comparison",
+            "link": "national-comparison",
+            "text": "To reach a CO₂ emissions peak by 2030 under baseline economic assumptions, China’s CO₂ intensity will need to fall between 4%-5%/year."
+        },
+        {
+            "title": "Economic growth",
+            "link": "economic-growth",
+            "text": "Slower economic growth means an earlier CO₂ emissions peak, less expansion of non-fossil energy"
+        },
+        {
+            "title": "CO₂ by province",
+            "link": "co2-by-province",
+            "text": "The effects of policy vary widely across China’s provinces, consistent with diverse features of provincial energy systems and economies."
+        },
+        {
+            "title": "Health impacts by province",
+            "link": "health-impacts-by-province",
+            "text": "Combining the spatial distribution of population with projections of air quality provide insight where the health consequences will be greatest."
+        },
+        {
+            "title": "Provincial comparison",
+            "link": "provincial-comparison",
+            "text": "The impacts of policy on provinces can be traced to differences in population, per-capita income, and reliance on coal"
+        }
+    ]
+    return template.render(page_index=pages)
